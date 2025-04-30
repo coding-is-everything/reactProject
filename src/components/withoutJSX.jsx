b3e1d7cc7116a6da5350b5656988adb9302e3578
The Batraa Numerology <thebatraanumerology@gmail.com>
2025-04-30 17:22:06 +0530

    New Changes in withoutJSX Logic File

--- src/components/withoutJSX.jsx
+++ src/components/withoutJSX.jsx
@@ -1,7 +1,12 @@
 import { createElement } from "react";
 
 function withoutJSX() {
-    return createElement("div", {id: "rootDiv"}, "Hello Div");
+    // return createElement("div", {id: "rootDiv"}, "Hello Div");
+    return (
+        <div className="rootDiv">
+            <h1>Hello Div</h1>
+        </div>
+    )
 }
 
 export default withoutJSX
