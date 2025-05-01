cdb47fbab638957020894b21030e40769c457918
The Batraa Numerology <thebatraanumerology@gmail.com>
2025-05-01 10:50:31 +0530

    Added new logic user variable with JSX

--- src/components/JSXWithCurlyBraces.jsx
+++ src/components/JSXWithCurlyBraces.jsx
@@ -1,7 +1,11 @@
 function JSXWithCurlyBraces(){
+
+    const name="Abhishek Shrivastava";
+
     return (
         <div>
             <h1>JSX with Curly Braces</h1>
+            <h1>{name}</h1>
         </div>
     )
 }
