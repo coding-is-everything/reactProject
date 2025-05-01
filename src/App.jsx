a349222538045c3d05f2860f3cc355c57811e6f3
The Batraa Numerology <thebatraanumerology@gmail.com>
2025-05-01 10:47:17 +0530

    New Modulle JSX With Curly Braces Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header"
 import JsxExcerse from "./components/jsxExcerse"
+import JSXWithCurlyBraces from "./components/JSXWithCurlyBraces"
 function App() {
   return(
     <div>
       <Header /> 
       <JsxExcerse />
+      <JSXWithCurlyBraces />
     </div>
   )
 }
---
+++ src/components/JSXWithCurlyBraces.jsx
@@ -0,0 +1,9 @@
+function JSXWithCurlyBraces(){
+    return (
+        <div>
+            <h1>JSX with Curly Braces</h1>
+        </div>
+    )
+}
+
+export default JSXWithCurlyBraces
