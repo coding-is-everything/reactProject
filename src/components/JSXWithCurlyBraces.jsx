3aa31e46f562909de1544e3ebb8acc98b6f96a6d
The Batraa Numerology <thebatraanumerology@gmail.com>
2025-05-01 10:57:18 +0530

    Added new user variable line

--- src/components/JSXWithCurlyBraces.jsx
+++ src/components/JSXWithCurlyBraces.jsx
@@ -1,11 +1,15 @@
 function JSXWithCurlyBraces(){
 
     const name="Abhishek Shrivastava";
+    let x=10;
+    let y=20;
 
     return (
         <div>
             <h1>JSX with Curly Braces</h1>
             <h1>{name}</h1>
+            <h1>{x+y}</h1>
+            <button onClick={() => alert("Hello World")}>Click Me</button>
         </div>
     )
 }
