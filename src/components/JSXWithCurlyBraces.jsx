fe19749841cad6f283996dea9f51e0c49e5739db
The Batraa Numerology <thebatraanumerology@gmail.com>
2025-05-01 11:10:22 +0530

    Parameterized function example

--- src/components/JSXWithCurlyBraces.jsx
+++ src/components/JSXWithCurlyBraces.jsx
@@ -8,12 +8,19 @@ function JSXWithCurlyBraces(){
     {
         return "Apple";
     }
+
+    //Parameterized Function
+    function sum(a,b) {
+        return a+b;
+    }
+
     return (
         <div>
             <h1>JSX with Curly Braces</h1>
             <h1>{name}</h1>
             <h1>{x+y}</h1>
             <h1>{fruit()}</h1>
+            <h1>{sum(10,100)}</h1>
             <button onClick={() => alert("Hello World")}>Click Me</button>
         </div>
     )
