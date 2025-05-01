74046824c56649678a75ca9818f583d9c9f8319c
The Batraa Numerology <thebatraanumerology@gmail.com>
2025-05-01 11:00:36 +0530

    Added function with JSX

--- src/components/JSXWithCurlyBraces.jsx
+++ src/components/JSXWithCurlyBraces.jsx
@@ -4,11 +4,16 @@ function JSXWithCurlyBraces(){
     let x=10;
     let y=20;
 
+    function fruit()
+    {
+        return "Apple";
+    }
     return (
         <div>
             <h1>JSX with Curly Braces</h1>
             <h1>{name}</h1>
             <h1>{x+y}</h1>
+            <h1>{fruit()}</h1>
             <button onClick={() => alert("Hello World")}>Click Me</button>
         </div>
     )
